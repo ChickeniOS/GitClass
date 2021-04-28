59a34282677259041ff8a54674775836c53a8230
Yuri Frota <yurisaboiaf@gmail.com>
2021-04-28 10:10:51 -0300

    First Commit

---
+++ Conflict/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Conflict
+//
+//  Created by Yuri on 28/04/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  @IBOutlet var redButton: UIButton!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func goToScreen(_ sender: UIButton) {
+    print("\(sender.tag) pressed")
+  }
+}
+
